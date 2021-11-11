b57e9d3b9f04f5dedbfb5f450f133dcc430d3b10
kevinliang91705 <s1091750@gm.pu.edu.tw>
2021-11-11 13:22:15 +0800

    make it better9

--- tc_flask.py
+++ tc_flask.py
@@ -5,14 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 	homepage = "<h1>410917504 資管二B 梁實翔</h1>"
-    homepage += "<a href=/kevin>MIS工作</a><br>"
+    homepage += "<a href=/aboutme>MIS工作</a><br>"
     return homepage
 
-@app.route("/kevin")
+@app.route("/aboutme")
 def mis2b():
-	return render_template("KEVIN.html")
-
-
+	return render_template("aboutme.html")
 
 if __name__ == "__main__":
     app.run()
