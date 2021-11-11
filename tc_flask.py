598bba7a76f993f5884178b6668826df3466e423
kevinliang91705 <s1091750@gm.pu.edu.tw>
2021-11-11 13:26:02 +0800

    make it better10

--- tc_flask.py
+++ tc_flask.py
@@ -12,5 +12,35 @@ def index():
 def mis2b():
 	return render_template("aboutme.html")
 
+@app.route("/mis")
+def course():
+    return "<h1>資訊管理導論</h1>"
+
+@app.route("/current")
+def current():
+    now = datetime.now()
+    return render_template("current.html", datetime = str(now))
+
+@app.route("/welcome", methods=["GET", "POST"])
+def welcome():
+    user = request.values.get("nick")
+    return render_template("welcome.html", name=user)
+
+@app.route('/hi')
+def hi():
+    # 載入原始檔案
+    f = open("count.txt", "r")
+    count = int(f.read())
+    f.close()
+
+    # 計數加1
+    count += 1
+
+    # 覆寫檔案
+    f = open("count.txt", "w")
+    f.write(str(count))
+    f.close()
+    return ("本網站總拜訪人次："+str(count))
+
 if __name__ == "__main__":
     app.run()
