d21ada189d14a754463147c41b8caa03a9dac2a3
kevinliang91705 <s1091750@gm.pu.edu.tw>
2021-11-11 13:15:04 +0800

    make it better8

--- tc_flask.py
+++ tc_flask.py
@@ -1,15 +1,15 @@
-from flask import Flask,render_template, request
+from flask import Flask, render_template, request
 from datetime import datetime
 app = Flask(__name__)
 
 @app.route("/")
 def index():
 	homepage = "<h1>410917504 資管二B 梁實翔</h1>"
-    //homepage += "<a href=/kevin>MIS工作</a><br>"
+    homepage += "<a href=/kevin>MIS工作</a><br>"
     return homepage
 
 @app.route("/kevin")
-def course():
+def mis2b():
 	return render_template("KEVIN.html")
 
 
